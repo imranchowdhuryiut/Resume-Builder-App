96af62aab29b91edb0c5073cbb86d84dbf9b5b5d
Imran Chowdhury <imranchowdhury@iut-dhaka.edu>
2022-02-19 00:47:37 +0600

    Application Class added

---
+++ app/src/main/java/com/example/resumebuilder/ResumeBuilderApp.kt
@@ -0,0 +1,13 @@
+package com.example.resumebuilder
+
+import android.app.Application
+
+/**
+ * Created by Imran Chowdhury on 2/19/2022.
+ */
+class ResumeBuilderApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
