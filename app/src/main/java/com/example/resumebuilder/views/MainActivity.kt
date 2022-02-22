50626bfd910fe7efbc1b1042d438d98ae4b54e2c
Imran Chowdhury <imranchowdhury@iut-dhaka.edu>
2022-02-22 16:31:30 +0600

    nav controller created

--- app/src/main/java/com/example/resumebuilder/views/MainActivity.kt
+++ app/src/main/java/com/example/resumebuilder/views/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.resumebuilder.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import com.example.resumebuilder.R
 import com.example.resumebuilder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         installSplashScreen()
         setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
